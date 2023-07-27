9a13bbb1b2e9dead117c31cc51b6e0d79506719e
GabrielCappelli <6148081+GabrielCappelli@users.noreply.github.com>
2023-07-27 17:48:15 +0100

    Adds test to cover container injection

--- tests/test_container_builder.py
+++ tests/test_container_builder.py
@@ -1,6 +1,6 @@
 import pytest
 
-from meta_di import ContainerBuilder, MetaDIException
+from meta_di import ContainerBuilder, ContainerProto, MetaDIException
 from tests.conftest import ISingletonService, SingletonService, TransientService
 
 
@@ -49,3 +49,28 @@ def test_build_container__when_passing_function_as_provider__then_the_function_i
     container = ContainerBuilder().add_singleton("123", get_123).build()
 
     assert container.get("123") == 123
+
+
+def get_container(container: ContainerProto):
+    return container
+
+
+def test_build_container__container_is_injected_as_singleton():
+    container = (
+        ContainerBuilder().add_singleton(SingletonService, get_container).build()
+    )
+
+    assert container.get(SingletonService) is container
+    assert container.get(SingletonService) is container.get(SingletonService)
+
+
+def test_build_container__scoped_container_is_injected_as_singleton():
+    container = (
+        ContainerBuilder().add_transient(SingletonService, get_container).build()
+    )
+
+    with container as scoped_container:
+        assert scoped_container.get(SingletonService) is scoped_container
+        assert scoped_container.get(SingletonService) is scoped_container.get(
+            SingletonService
+        )
